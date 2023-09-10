f412b2e2b14db74b3ac6def9ee3479f6fb8f4b4d
Yurii Chobaniuk <sonicmaster172@gmail.com>
2023-09-10 12:27:07 +0300

    feat(mappings): updated mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,8 +3,9 @@ require 'mapx'.setup { global = true }
 map('U', '<C-R>', { silent = true })
 map('<leader>y', '"+y', 'silent')
 map('<leader>p', '"+p', 'silent')
-map ('<leader>d', '"_d', 'silent')
--- nnoremap gb <C-O>
--- xnoremap gb <C-O>
--- xnoremap gf <C-I>
--- nnoremap gf <C-I>
+map('<leader>d', '"_d', 'silent')
+map('<leader>q', ':q!<Cr>', 'silent')
+map('<leader>w', ':wq<Cr>', 'silent')
+map('q', '', 'silent')
+map('gb', '<C-o>', 'silent')
+map('gf', '<C-i>', 'silent')
